38bc257ebbb4b742c2178f1ab0666f4fa8b621e6
Los Vitaly <38357863+vitaly-los@users.noreply.github.com>
2018-10-15 21:28:58 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget --no-check-certificate https://raw.githubusercontent.com/tao12345666333/vim/master/vimrc -O $HOME/.vimrc
-
-vim -E -u $HOME/.vimrc +qall
-
-echo 'Install Complete! '
